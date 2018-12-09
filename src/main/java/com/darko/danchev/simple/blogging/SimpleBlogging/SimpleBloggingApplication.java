7539b1ee6a32696af076629bc268321da04309a6
Darko Danchev <dancevdarko@gmail.com>
2018-12-09 22:05:07 +0200

    Initialize

---
+++ src/main/java/com/darko/danchev/simple/blogging/SimpleBlogging/SimpleBloggingApplication.java
@@ -0,0 +1,12 @@
+package com.darko.danchev.simple.blogging.SimpleBlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBloggingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/darko/danchev/simple/blogging/SimpleBlogging/SimpleBloggingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.darko.danchev.simple.blogging.SimpleBlogging;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleBloggingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
