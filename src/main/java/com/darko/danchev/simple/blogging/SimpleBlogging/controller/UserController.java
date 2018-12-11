f3d8f35e31549f4ed8cd5d23aaa448f3f24eeee3
Darko Danchev <dancevdarko@gmail.com>
2018-12-11 14:23:40 +0200

    Remove oauth2 and user system

--- src/main/java/com/darko/danchev/simple/blogging/SimpleBlogging/controller/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.darko.danchev.simple.blogging.SimpleBlogging.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/users")
-public class UserController {
-}
