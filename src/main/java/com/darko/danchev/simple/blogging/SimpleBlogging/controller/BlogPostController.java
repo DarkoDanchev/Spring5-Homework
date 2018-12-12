c623e89ed21f9bb202d7692ae7ff149440e764df
Darko Danchev <dancevdarko@gmail.com>
2018-12-12 02:41:58 +0200

    Expose Services to Controllers

--- src/main/java/com/darko/danchev/simple/blogging/SimpleBlogging/controller/BlogPostController.java
+++ src/main/java/com/darko/danchev/simple/blogging/SimpleBlogging/controller/BlogPostController.java
@@ -1,9 +1,14 @@
 package com.darko.danchev.simple.blogging.SimpleBlogging.controller;
 
+import com.darko.danchev.simple.blogging.SimpleBlogging.service.abstraction.BlogPostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/blogposts")
 public class BlogPostController {
+
+    @Autowired
+    BlogPostService blogPostService;
 }
--- src/main/java/com/darko/danchev/simple/blogging/SimpleBlogging/controller/TagController.java
+++ src/main/java/com/darko/danchev/simple/blogging/SimpleBlogging/controller/TagController.java
@@ -1,9 +1,14 @@
 package com.darko.danchev.simple.blogging.SimpleBlogging.controller;
 
+import com.darko.danchev.simple.blogging.SimpleBlogging.service.abstraction.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/tags")
 public class TagController {
+
+    @Autowired
+    TagService tagService;
 }
